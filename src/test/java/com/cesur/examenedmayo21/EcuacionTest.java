6a3066b5a0bc208b7068afb3fc4c5f430ab2ce80
FRomero999 <77333581+FRomero999@users.noreply.github.com>
2021-05-13 11:44:20 +0200

    Update EcuacionTest.java

--- src/test/java/com/cesur/examenedmayo21/EcuacionTest.java
+++ src/test/java/com/cesur/examenedmayo21/EcuacionTest.java
@@ -17,23 +17,21 @@ public class EcuacionTest {
     @DisplayName("Evaluar la ecuación para un valor")
     public void testEvaluar() {
         System.out.println("evaluar");
-        //Double expResult, result;
-        //e = new Ecuacion(1,0,0);        
-        // assertEquals(expResult, result, 0.005);
+        /* Completar */
         fail("Test no implementado aun");
     }
 
     
     @Test
-    @DisplayName("Solo el parametro A")   
+    @DisplayName("Resolver ecuación de segundo grado")   
     public void testResolver() {
-        System.out.println("Solo hay A");
+        System.out.println("Resolver ecuación");
 
         e = new Ecuacion(1,0,0);        
         e.resolver();
-                
-        assertEquals(e.getSolucion1(),0,0.1);        
-        assertEquals(e.getSolucion2(),0,0.1);        
+        /* Completar */
+        
+        fail("Test no implementado aun");                
         
     }
 
