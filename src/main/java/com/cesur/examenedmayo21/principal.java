90c354ebec0aaa279033940d360ca7da89bb9e67
FRomero999 <77333581+FRomero999@users.noreply.github.com>
2021-05-13 11:46:19 +0200

    Update principal.java

--- src/main/java/com/cesur/examenedmayo21/principal.java
+++ src/main/java/com/cesur/examenedmayo21/principal.java
@@ -1,19 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.cesur.examenedmayo21;
 
-/**
- *
- * @author paco
- */
 public class principal {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
 
          Ecuacion e = new Ecuacion(2,0,-4);
